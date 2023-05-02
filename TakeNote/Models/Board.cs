3e60504ef628024054163111ba707e9a5583c970
Rida-tarabishi <105437991+Rida-tarabishi@users.noreply.github.com>
2023-05-01 20:56:50 -0400

    Added Models for Board and Note

---
+++ TakeNote/Models/Board.cs
@@ -0,0 +1,31 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ColumnAttribute = SQLite.ColumnAttribute;
+using TableAttribute = SQLite.TableAttribute;
+
+namespace TakeNote.Models
+{
+    [Table("boards")]
+    public class Board
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<Note> notes { get; set; }
+
+        public Board()
+        {
+            this.Id = 0;
+            this.Name = "Default";
+        }
+    }
+}
---
+++ TakeNote/Models/Note.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColumnAttribute = SQLite.ColumnAttribute;
+using ForeignKeyAttribute = SQLiteNetExtensions.Attributes.ForeignKeyAttribute;
+using TableAttribute = SQLite.TableAttribute;
+
+namespace TakeNote.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+        public string Title { get; set; }
+
+
+        [ForeignKey(typeof(Board))]
+        public int boardID { get; set; }
+
+
+        public Note()
+        {
+            
+
+        }
+
+        [ManyToOne]
+        public Board board { get; set; }
+
+    }
+}
