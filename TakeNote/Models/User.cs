f123db447b9a0b900d9bd34f911aee38856ebdae
DuckD <DuckD@DESKTOP-5FDV79F>
2023-05-07 17:59:18 -0700

    Added User Class

---
+++ TakeNote/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeNote.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public User() { }
+
+    }
+}
