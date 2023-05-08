bef0acfaaf489d96ab3864531b871402f651f670
Rida-tarabishi <105437991+Rida-tarabishi@users.noreply.github.com>
2023-05-08 07:42:56 -0400

    Added Popup for crearing a new board

---
+++ TakeNote/Components/createBoardPopUp.xaml.cs
@@ -0,0 +1,12 @@
+
+using CommunityToolkit.Maui.Views;
+
+namespace TakeNote.Components;
+
+public partial class createBoardPopUp : Popup
+{
+	public createBoardPopUp()
+	{
+		InitializeComponent();
+	}
+}
