00ca015cf7565e562eeecf301a3404a2a23f407c
Rida-tarabishi <105437991+Rida-tarabishi@users.noreply.github.com>
2023-05-08 07:43:44 -0400

    Updated Routing Hidden Register Flyout

--- TakeNote/AppShell.xaml.cs
+++ TakeNote/AppShell.xaml.cs
@@ -8,5 +8,6 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
         Routing.RegisterRoute("BoardsView", typeof(BoardsView));
+        Routing.RegisterRoute("RegisterPage", typeof(RegisterPage));
     }
 }
