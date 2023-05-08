590dc1ebafc4f3793a2e3e3c043001a8a380d57c
Rida-tarabishi <105437991+Rida-tarabishi@users.noreply.github.com>
2023-05-08 07:45:52 -0400

    Added Maui Toolkit

--- TakeNote/MauiProgram.cs
+++ TakeNote/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using TakeNote.Data;
 
 namespace TakeNote;
@@ -9,7 +10,7 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
-			.UseMauiApp<App>()
+			.UseMauiApp<App>().UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
